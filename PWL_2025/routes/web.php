62cd3da2918bb6ddaff9e215e202db1c65f4b9ea
GhoffarFitassin <ghoffar8804@gmail.com>
2025-02-19 10:10:50 +0700

    update: route parameters

--- PWL_2025/routes/web.php
+++ PWL_2025/routes/web.php
@@ -17,3 +17,17 @@ Route::get('/world', function () {
 Route::get('/about', function () {
     return '2341720035 Ghoffar Abdul';
 });
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function
+($articlesId,) {
+return 'Halaman Artikel dengan ID '.$articlesId;
+});
