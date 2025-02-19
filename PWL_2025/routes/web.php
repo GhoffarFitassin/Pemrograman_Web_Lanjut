f8589d5ad2dc4a1e88277dd9a371756a7e34e868
GhoffarFitassin <ghoffar8804@gmail.com>
2025-02-19 10:28:22 +0700

    update: optional parameters

--- PWL_2025/routes/web.php
+++ PWL_2025/routes/web.php
@@ -2,32 +2,40 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
 
-Route::get('/hello', function () {
-    return 'Hello world';
-});
+// Route::get('/hello', function () {
+//     return 'Hello world';
+// });
 
-Route::get('/world', function () {
-    return 'World';
-});
+// Route::get('/world', function () {
+//     return 'World';
+// });
 
-Route::get('/about', function () {
-    return '2341720035 Ghoffar Abdul';
-});
+// Route::get('/about', function () {
+//     return '2341720035 Ghoffar Abdul';
+// });
 
-Route::get('/user/{name}', function ($name) {
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '.$name;
+// });
+
+// Route::get('/posts/{post}/comments/{comment}', function
+// ($postId, $commentId) {
+// return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+// });
+
+// Route::get('/articles/{id}', function
+// ($articlesId,) {
+// return 'Halaman Artikel dengan ID '.$articlesId;
+// });
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+// });
+
+Route::get('/user/{name?}', function ($name='Jhon') {
     return 'Nama saya '.$name;
-});
-
-Route::get('/posts/{post}/comments/{comment}', function
-($postId, $commentId) {
-return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
-
-Route::get('/articles/{id}', function
-($articlesId,) {
-return 'Halaman Artikel dengan ID '.$articlesId;
 });
