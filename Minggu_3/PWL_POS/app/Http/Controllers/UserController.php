3da4aa269969cde0a9cc69d495cc4cdbd9a4d4a8
GhoffarFitassin <ghoffar8804@gmail.com>
2025-03-07 13:28:00 +0700

    update: Praktikum 2.2 – Not Found Exceptions

--- Minggu_3/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu_3/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,18 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $user = UserModel::find(1);
-        // $user = UserModel::where('level_id', 1)->first();
-        // $user = UserModel::firstWhere('level_id', 1);
-        // return view ('user',['data'=>$user]);
-        // $user = UserModel::findor(1, ['username', 'nama',], function () {
-        //         abort(404);
-        //     }
-        // );
-        $user = UserModel::findor(20, ['username', 'nama',], function () {
-                abort(404);
-            }
-        );
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username','manager99')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
