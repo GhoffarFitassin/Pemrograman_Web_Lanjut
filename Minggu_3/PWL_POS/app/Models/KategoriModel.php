169f1a06c9e75da539127b4ddfb188790c4ca5fa
GhoffarFitassin <ghoffar8804@gmail.com>
2025-03-19 06:21:09 +0700

    update: Pertemuan 5 - Tugas - Implementasi pada kategori barang

---
+++ Minggu_3/PWL_POS/app/Models/KategoriModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_kategori'; // mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'kategori_id'; // mendefinisikan primary key tabel
+    protected $fillable = ['kategori_kode', 'nama']; // mendefinisikan kolom yang dapat diisi oleh model ini
+}
