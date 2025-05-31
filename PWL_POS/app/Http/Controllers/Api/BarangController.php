c943cb5d5221d637c0ceddc79612070517108bfe
GhoffarFitassin <ghoffar8804@gmail.com>
2025-05-31 20:28:20 +0700

    update: tugas jobsheet 10

--- PWL_POS/app/Http/Controllers/Api/BarangController.php
+++ PWL_POS/app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -15,7 +16,34 @@ class BarangController extends Controller
 
     public function store(Request $request)
     {
-        $barang = BarangModel::create($request->all());
+        // set validation
+        $validator = Validator::make($request->all(), [
+            'kategori_id' => 'required',
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        // if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        
+        // handle image upload
+        $image = $request->file('image');
+        $image->store('public/images');
+
+        // create user
+        $barang = BarangModel::create([
+            'kategori_id' => $request->kategori_id,
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama, 
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'image' => $image->hashName(),
+        ]);
         return response()->json($barang, 201);
     }
 
--- PWL_POS/app/Models/BarangModel.php
+++ PWL_POS/app/Models/BarangModel.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject; 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
-class BarangModel extends Model
+
+class BarangModel extends Model implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
+
     use HasFactory;
 
     protected $table = 'm_barang'; // mendefinisikan nama tabel yang digunakan oleh model ini
@@ -21,10 +33,18 @@ class BarangModel extends Model
         'barang_kode',
         'barang_nama',
         'harga_beli',
-        'harga_jual'
+        'harga_jual',
+        'image',
     ];
 
     public function kategori() {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+     protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
